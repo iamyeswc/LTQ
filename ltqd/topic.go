82e7bfe1b0ab3baf9972325d8827f98d02fd6667
Yaowei <1968486507@qq.com>
2025-04-29 10:42:46 +0800

    task: constructor start messagePump of topic

--- ltqd/topic.go
+++ ltqd/topic.go
@@ -8,7 +8,10 @@ type Topic struct {
 	channels       map[string]*Channel
 }
 
-// construtor of Topic
+// constructor of Topic
+// - 初始化topic中的各个成员变量
+// - 启动messagePump协程 利用sync.WaitGroup做同步
+// - 通知ltqd 触发新topic建立时 ltqd需要做的工作(metadata持久化到磁盘、通知lookup注册了新的topic)
 func NewTopic() *Topic {
 
 	go messagePump()
@@ -16,6 +19,16 @@ func NewTopic() *Topic {
 	return &Topic{}
 }
 
+// Start
+// - 控制topic的开启 开始接收消息(控制messagePump的开启)
+// - 通过一个startChan来做goroutine之间的消息传递
+func Start() {
+	
+}
+
+// messagePump
+// - 当前topic的消息处理函数
+// - 不断select接收来自in-memory queue和backend queue的消息 并复制到当前topic的每个channel
 func messagePump() {
 
 }
